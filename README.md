193c83aeabbf164c857b544bdf9852efd0d12483
mj <mjsolidarios@wvsu.edu.ph>
2015-09-13 21:56:44 +0800

    Added MDL to shuffler

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 html_bingocard
 ==============
 
-Simple BINGO card written in HTML that uses random integers and arrays.
+Simple BINGO card and shuffler written in HTML, CSS and JavaScript utilizing Fisher-Yates shuffle algorithm. Inspired by Google's Material Design Lite.
--- js/bingo-shuffler.js
+++ js/bingo-shuffler.js
@@ -6,17 +6,29 @@ var initializedComboSet = [];
 
 var btn = document.querySelector('button');
 var h1Element = document.querySelector('h1');
-var itemCountElement = document.querySelector('span');
+var itemCountElement = document.querySelector('span.count');
+var itemDataTable = document.querySelector('tbody');
 
 var combo = bingo.generateCombo(bingo.generalSet(initializedGeneralSet));
-
+var countCounter = 1;
 btn.onclick = function () {
   var pop = combo.pop();
   console.log("Poped-out " + pop);
     if (combo.length > 0) {
         h1Element.innerHTML = pop;
+        var tr = document.createElement('tr');
+        var tdIndex = document.createElement('td');
+        var tdCombo = document.createElement('td');
+        tdIndex.className = 'mdl-data-table__cell--non-numeric';
+        tdIndex.appendChild(document.createTextNode(countCounter));
+        tdCombo.className = 'mdl-data-table__cell--non-numeric';
+        tdCombo.appendChild(document.createTextNode(pop));
+        tr.appendChild(tdIndex);
+        tr.appendChild(tdCombo);
+        itemDataTable.appendChild(tr);
+        countCounter++;
     } else {
         h1Element.innerHTML = "EMPTY";
     }
-    itemCountElement.innerHTML = combo.length;
+    itemCountElement.setAttribute("data-badge",combo.length);
 };
